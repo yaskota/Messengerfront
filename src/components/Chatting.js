f3fb7ae645dd3677c0207d12ea92427ada6f3801
yaskota <147639564+yaskota@users.noreply.github.com>
2025-05-04 10:59:24 +0530

    Update Chatting.js

--- src/components/Chatting.js
+++ src/components/Chatting.js
@@ -1,4 +1,3 @@
-// components/Chatting.js
 import React, { useState } from 'react';
 import UserList from './UserList';
 import ChatPage from './ChatPage';
@@ -13,31 +12,33 @@ function Chatting() {
     setShowSidebar(false); // Hide sidebar on mobile
   };
 
+  const handleBack = () => {
+    setShowSidebar(true);   // Show sidebar again
+    setSelectedUser(null);  // Optional: clear selected user
+  };
+
   return (
     <div className="flex h-[calc(100vh-75px)] w-full overflow-hidden">
-      {/* Sidebar */}
-      <div
-        className={`${
-          showSidebar ? 'block' : 'hidden'
-        } md:block w-full md:w-1/3 lg:w-1/4 border-r bg-white`}
-      >
-        <UserList
-          onUserSelect={handleUserSelect}
-          selectedUser={selectedUser}
-        />
-      </div>
+      {/* Sidebar (UserList) */}
+      {(!selectedUser || showSidebar) && (
+        <div className="w-full md:w-1/3 lg:w-1/4 border-r bg-white">
+          <UserList onUserSelect={handleUserSelect} selectedUser={selectedUser} />
+        </div>
+      )}
 
-      {/* Chat view */}
-      <div className="flex-1">
-        {selectedUser ? (
-          <ChatPage
-            selectedUser={selectedUser}
-            onBack={() => setShowSidebar(true)}
-          />
-        ) : (
+      {/* Chat View (ChatPage or Default) */}
+      {selectedUser && !showSidebar && (
+        <div className="flex-1">
+          <ChatPage selectedUser={selectedUser} onBack={handleBack} />
+        </div>
+      )}
+
+      {/* Large screen default view when no user is selected */}
+      {!selectedUser && !showSidebar && (
+        <div className="flex-1 hidden md:flex">
           <Default />
-        )}
-      </div>
+        </div>
+      )}
     </div>
   );
 }
